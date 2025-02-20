4d33aaa3d7931cee160d9e45a7767fb29e83e104
Testa De Nevill <testadenevill@gmail.com>
2025-02-19 23:46:36 -0800

    I did something, i dont know what

--- src/components/GoogleSheetsAPI.js
+++ src/components/GoogleSheetsAPI.js
@@ -5,6 +5,10 @@ const SHEET_NAME = "Sheet1";  // Change this if needed
 
 export async function submitToGoogleSheets(data) {
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${SHEET_NAME}!A:D:append?valueInputOption=USER_ENTERED&key=${API_KEY}`;
+//     `https://sheets.googleapis.com/v4/spreadsheets/1NU6_USQ_aEvXVlptsrB4NP0fCJOGuy9MXOBU_Q8v8K4/values/Sheet1!A:D:append?valueInputOption=USER_ENTERED&key=AIzaSyCYB9rp7s_ueHlKdgJLVAlupPyseBAe3k0`
+    
+//     REACT_APP_GOOGLE_SHEET_ID="1NU6_USQ_aEvXVlptsrB4NP0fCJOGuy9MXOBU_Q8v8K4"
+// REACT_APP_GOOGLE_API_KEY="AIzaSyCYB9rp7s_ueHlKdgJLVAlupPyseBAe3k0"
 
     const values = [[data.name, data.email, data.message, data.subscribe ? "Yes" : "No"]];
 
